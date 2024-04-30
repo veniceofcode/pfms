f8cde5ffa13c22abb8f1c6347c67ef4eaaff0e9f
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-04-30 13:54:19 -0400

    Add pfms specific updates

--- pfms/config/settings.py
+++ pfms/config/settings.py
@@ -1,21 +1,37 @@
-import  os
-from    typing              import Any
-from    pydantic            import AnyHttpUrl, AnyUrl
-from    pydantic_settings   import BaseSettings
-from    pftag               import pftag
+import os
+from typing import Any
+from pydantic import AnyHttpUrl, AnyUrl
+from pydantic_settings import BaseSettings
+from pftag import pftag
+from pathlib import Path
+
+
+class ModelMeta(BaseSettings):
+    location: Path = Path.home() / "spleenseg" / "models"
+
+
+class AnalysisMeta(BaseSettings):
+    location: Path = Path.home() / "spleenseg" / "analysis"
 
 
 class Vault(BaseSettings):
-    locked:bool             = False
-    vaultKey:str            = ''
+    locked: bool = False
+    vaultKey: str = ""
+
 
 class Credentials(BaseSettings):
-    username:str            = 'chris'
-    password:str            = 'chris1234'
+    username: str = "chris"
+    password: str = "chris1234"
 
-def vaultCheckLock(vault:Vault) -> None:
+
+def vaultCheckLock(vault: Vault) -> None:
     if vault.vaultKey and not vault.locked:
-        vault.locked        = True
+        vault.locked = True
         print("Vault check: key has already been set. Vault is now LOCKED.")
 
-vault               = Vault()
+
+vault = Vault()
+modelMeta = ModelMeta()
+
+analysisMeta = AnalysisMeta()
+# analysisMeta.location.mkdir(parents=True, exist_ok=True)
